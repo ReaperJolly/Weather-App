6491e4ffb6ce5d35e426c26ea829681b714d50d7
Dragan Kukolj <102326133+ReaperJolly@users.noreply.github.com>
2023-04-08 22:58:16 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,37 +3,64 @@ import axios from 'axios';
 
 function App() {
 
-  const GEO_URL = 'https://api.openweathermap.org/geo/1.0/zip?zip=31000,HR&appid=c77cdcea84c788c94cee82c4437464ee'
+  const [ data, setData ] = useState(null);
+  const [ location, setLocation ] = useState("");
+
+  const apiKey = 'c77cdcea84c788c94cee82c4437464ee';
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`)
+        .then((response) => {
+          setData(response.data)
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
 
   return (
     <div className="weather-app">
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder="Enter your city"
+        type="text" 
+        />
+      </div>
+      {data &&
       <div className="container">
         <div className="upper-element">
           <div className="location">
-            <p>Osijek</p>
+            <p>{data.name}, {data.sys.country}</p>
           </div>
           <div className="temperature">
-            <h1>23°C</h1>
+            <h1>{Math.round(data.main.temp)}°C</h1>
           </div>
           <div className="description">
-            <p>Clear</p>
+            <p>{data.weather[0].description}</p>
           </div>
         </div>
         <div className="bottom-element">
           <div className="feel">
-            <p className="bold">20°C</p>
+            <p className="bold">{Math.round(data.main.feels_like)}°C</p>
             <p id="p-description">Real feel</p>
           </div>
           <div className="humidity">
-            <p className="bold">20%</p>
+            <p className="bold">{data.main.humidtiy}%</p>
             <p id="p-description">Humidity</p>
           </div>
           <div className="wind-speed">
-            <p className="bold">2m/s</p>
+            <p className="bold">{data.wind.speed}m/s</p>
             <p id="p-description">Wind speed</p>
           </div>
         </div>
       </div>
+      };  
     </div>
   );
 }
